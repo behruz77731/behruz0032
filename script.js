47f98d789545e72c7c4379e0d467fd0179da66e6
behruz77731 <sekrtubehruz@gmail.com>
2025-04-19 15:58:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const name = 'behruz';
+console.log(typeof(5555));
+
+let box =prompt ('Привет как тебе завут')
+
+let box1 =prompt('сколько тебе лет')
+
+let box2 =prompt('гдй ты живёшь')
+
+alert(`мне завут ${box} мне лет ${box1} я живу ${box2}`)
